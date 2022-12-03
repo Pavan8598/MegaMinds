5c0ab832f0e11a78575d221678953e56d5d0857b
milin <milin@LAPTOP-GF2BNUVG>
2022-12-03 22:57:24 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        masterEntities db = new masterEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetUserList()
+        {
+           
+
+            List<User> userList = db.Users.ToList();
+            return Json(userList, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        public JsonResult GetUserById(int Id)
+        {
+            User model = db.Users.Where(x => x.Id == Id).SingleOrDefault();
+            string value = string.Empty;
+            value = JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return Json(value, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public JsonResult SaveDataInDatabase(User model)
+        {
+            var result = false;
+            try
+            {
+                if (model.Id > 0)
+                {
+                    User us = db.Users.SingleOrDefault(x =>  x.Id == model.Id);
+                    us.Name = model.Name;
+                    us.Email = model.Email;
+                    us.Phone_no = model.Phone_no;
+                    us.City = model.City;
+                    us.State = model.State;
+                    us.Address = model.Address;
+                    us.Agree = model.Agree;
+                   
+                    db.SaveChanges();
+                    result = true;
+                }
+                else
+                {
+                    User us = new User();
+                    us.Name = model.Name;
+                    us.Email = model.Email;
+                    us.Phone_no = model.Phone_no;
+                    us.City = model.City;
+                    us.State = model.State;
+                    us.Address = model.Address;
+                    us.Agree = model.Agree;
+                    db.Users.Add(us);
+                    db.SaveChanges();
+      
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+        public JsonResult DeleteUserRecord(int Id)
+        {
+            bool result = false;
+
+           User us = db.Users.SingleOrDefault(x => x.Id ==Id);
+            db.Users.Remove(us);
+           
+               
+             db.SaveChanges();
+             result = true;
+           
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/UserMetadata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+    }
+    public class UserMetadata
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+      
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+       
+        [Required]
+        public string Phone_no { get; set; }
+        public string Address { get; set; }
+        [Required]
+       public string State { get; set; }
+        
+        [Required]
+        public string City { get; set; }
+        
+        [Required]
+        public bool Agree { get; set; }
+    }
+}
